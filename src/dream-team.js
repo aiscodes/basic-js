5c8c5268b2a37bb86e355abce98d47470001e263
aiscodes <aigerim6769@gmail.com>
2022-05-02 02:07:48 +0600

    feat: in process

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,23 +1,32 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require('../extensions/index.js')
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  // throw new NotImplementedError('Not implemented');
+  let name = ''
+  let arr = []
+  if (members instanceof Array) {
+    members.map((el) => {
+      if (typeof el == 'string') arr.push(el.split(' ').join('').toUpperCase())
+    })
+    const a = arr.sort()
+    a.map((el) => {el.length !==0 ? name += el[0] : name= false })
+  }
+  return name
 }
 
 module.exports = {
-  createDreamTeam
-};
+  createDreamTeam,
+}
--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,58 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason( date ) {
+  season = 'unknown';
+  if(date!==undefined){
+    month = date.toString().slice(4,7)
+    switch(month) {
+        case 'Dec':
+        // case 'december':
+        // case '12':
+        case 'Jan':
+        // case 'january':
+        // case '1':
+        case 'Feb':
+        // case 'february':
+        // case '2':
+            season = 'winter';
+        break;
+        case 'Mar':
+        // case 'march':
+        // case '3':
+        case 'Apr':
+        // case 'april':
+        // case '4':
+        case 'May':
+        // case '5':
+            season = 'spring';
+        break;
+        case 'Jun':
+        // case 'june':
+        // case '6':
+        case 'Jul':
+        // case 'july':
+        // case '7':
+        case 'Aug':
+        // case 'august':
+        // case '8':
+            season = 'summer';
+        break;
+        case 'Sep':
+        // case 'september':
+        // case '9':
+        case 'Oct':
+        // case 'october':
+        // case '10':
+        case 'Nov':
+        // case 'november':
+        // case '11':
+            season = 'fall';
+        break;
+    }
+   
+  }
+  return season !== 'unknown' ? season : "Invalid date!"
 }
 
 module.exports = {
