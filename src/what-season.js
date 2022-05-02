5a1ee45fbdc33dff264bca80ee1d8f7701705865
aiscodes <aigerim6769@gmail.com>
2022-05-02 10:14:45 +0600

    feat: in process

--- src/what-season.js
+++ src/what-season.js
@@ -12,58 +12,58 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function getSeason( date ) {
-  throw new NotImplementedError('Not implemented');
-  // season = '';
-  // if(date!==undefined){
-  //   month = date.toString().slice(4,7)
-  //   switch(month) {
-  //       case 'Dec':
-  //       // case 'december':
-  //       // case '12':
-  //       case 'Jan':
-  //       // case 'january':
-  //       // case '1':
-  //       case 'Feb':
-  //       // case 'february':
-  //       // case '2':
-  //           season = 'winter';
-  //       break;
-  //       case 'Mar':
-  //       // case 'march':
-  //       // case '3':
-  //       case 'Apr':
-  //       // case 'april':
-  //       // case '4':
-  //       case 'May':
-  //       // case '5':
-  //           season = 'spring';
-  //       break;
-  //       case 'Jun':
-  //       // case 'june':
-  //       // case '6':
-  //       case 'Jul':
-  //       // case 'july':
-  //       // case '7':
-  //       case 'Aug':
-  //       // case 'august':
-  //       // case '8':
-  //           season = 'summer';
-  //       break;
-  //       case 'Sep':
-  //       // case 'september':
-  //       // case '9':
-  //       case 'Oct':
-  //       // case 'october':
-  //       // case '10':
-  //       case 'Nov':
-  //       // case 'november':
-  //       // case '11':
-  //           season = 'fall';
-  //       break;
-  //   }
+  // throw new NotImplementedError('Not implemented');
+  season = '';
+  if(date!==undefined){
+    month = date.toString().slice(4,7)
+    switch(month) {
+        case 'Dec':
+        // case 'december':
+        // case '12':
+        case 'Jan':
+        // case 'january':
+        // case '1':
+        case 'Feb':
+        // case 'february':
+        // case '2':
+            season = 'winter';
+        break;
+        case 'Mar':
+        // case 'march':
+        // case '3':
+        case 'Apr':
+        // case 'april':
+        // case '4':
+        case 'May':
+        // case '5':
+            season = 'spring';
+        break;
+        case 'Jun':
+        // case 'june':
+        // case '6':
+        case 'Jul':
+        // case 'july':
+        // case '7':
+        case 'Aug':
+        // case 'august':
+        // case '8':
+            season = 'summer';
+        break;
+        case 'Sep':
+        // case 'september':
+        // case '9':
+        case 'Oct':
+        // case 'october':
+        // case '10':
+        case 'Nov':
+        // case 'november':
+        // case '11':
+            season = 'fall';
+        break;
+    }
    
-  // }
-  // return season !== '' ? season : "Invalid date!"
+  }
+  return season !== '' ? season : "Invalid date!"
 }
 
 module.exports = {
